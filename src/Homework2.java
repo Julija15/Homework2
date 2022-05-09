d92d162698c16cf0b6c4c8a53d5fa8724cd3eb0e
Julija15 <1771808@mail.ru>
2022-05-09 18:36:19 +0300

    1234

---
+++ src/Homework2.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Homework2 {
+    public static void main(String[] args) {
+        System.out.println("Homework2/1");
+        Scanner scanner = new Scanner(System.in);
+        int a, b, c, a1, a2, a3, b1, b2;
+        a = 100;
+        a1 = a / 100;
+        a3 = a1 * 75;
+        b = 7;
+        b1 = 7 % 5;
+        a2 = a3 * 10;
+        b2 = b1 * 2;
+        c = a2 + b2;
+        System.out.println(c);
+        int a4, a5, b3, c2;
+        a4 = a1 * 50;
+        a5 = a4 * 7;
+        b3 = b1 * 1;
+        c2 = a5 + b3;
+        System.out.println(c2);
+        int a6, a7, b4, c3;
+        a6 = a1 * 90;
+        a7 = a6 * 2;
+        b4 = b1 * 3;
+        c3 = a7 + b4;
+        System.out.println(c3);
+
+
+
+
+    }
+}
